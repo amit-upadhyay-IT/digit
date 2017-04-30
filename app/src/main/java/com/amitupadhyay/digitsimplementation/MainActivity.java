a0c157e89cd66e0595a86c105832c548847aa4dd
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-04-30 18:11:38 +0530

    gradle files added

--- app/src/main/java/com/amitupadhyay/digitsimplementation/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/digitsimplementation/MainActivity.java
@@ -1,13 +1,20 @@
 package com.amitupadhyay.digitsimplementation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button verifyPhoneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verifyPhoneBtn = (Button) findViewById(R.id.veryfy_phone_btn);
+
+
     }
 }
